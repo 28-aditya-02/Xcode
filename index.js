7021682534c774ffd7bb7f5b9e4ff05c4c8ffe10
28-aditya-02 <adityasingh0999@outlook.com>
2024-12-13 12:43:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let name = `Aditya Singh`;
+let age = 18;
+let mood = `not intrested`;
+
+document.getElementById("p1").textContent = name;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = mood;
+
